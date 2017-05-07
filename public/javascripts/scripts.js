fa39279d918124b72ddab48cc683e689ea9daa23
athithyaa <atpa2251@colorado.edu>
2017-05-06 22:32:55 -0600

    adding layer selector

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,15 @@
+function addLayer(name) {
+    $("<div>" + name + "</div>").insertBefore("#out");
+    $(".container").shapeshift({maxColumns: 1,selector: "*:not('.excl')"});
+}
+
 $(document).ready(function () {
-    $(".container").shapeshift({maxColumns: 1,animate: false});
+    $(".container").shapeshift({maxColumns: 1,selector: "*:not('.excl')"});
+    var cnt = 0;
+    $("#layerbutton").click(function () {
+        cnt+=1;
+        if(cnt <= 3)
+            addLayer($("#layerpicker").val());
+    })
+
 });
